51e461c440f3fa48005671dc1ea0da1ea40dbc82
mrtovar10 <mrtovar10@gmail.com>
2023-01-15 19:46:50 -0400

    primer comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface List{
+  nombrePokemon:string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'PruebaAngular';
+  public listPokemons: List[]=[];
+  ngOnInit():void {
+    this.listPokemons = [
+      {nombrePokemon:'ElPikapi'},
+      {nombrePokemon:'Charizarcox'},
+      {nombrePokemon:'Vupix'},
+      {nombrePokemon:'VamoaCalmano'},
+    ]
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchBarComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
